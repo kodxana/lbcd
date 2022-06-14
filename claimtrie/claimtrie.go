fdb3edd058704773c3e2e15d9080cbbcd758bbb0
Jonathan Moody <103143855+moodyjon@users.noreply.github.com>
2022-06-14 13:06:45 -0400

    [lbry] claimtrie: Adjust and rename blockProgressLogger -> claimProgressLogger.

--- claimtrie/claimtrie.go
+++ claimtrie/claimtrie.go
@@ -49,6 +49,9 @@ type ClaimTrie struct {
 
 	// Registrered cleanup functions which are invoked in the Close() in reverse order.
 	cleanups []func() error
+
+	// claimLogger communicates progress of claimtrie rebuild.
+	claimLogger *claimProgressLogger
 }
 
 func New(cfg config.Config) (*ClaimTrie, error) {
@@ -345,15 +348,19 @@ func (ct *ClaimTrie) ResetHeight(height int32) error {
 func (ct *ClaimTrie) runFullTrieRebuild(names [][]byte, interrupt <-chan struct{}) {
 	var nhns chan NameHashNext
 	if names == nil {
-		node.LogOnce("Building the entire claim trie in RAM...")
-
+		node.Log("Building the entire claim trie in RAM...")
+		ct.claimLogger = newClaimProgressLogger("Processed", node.GetLogger())
 		nhns = ct.makeNameHashNext(nil, true, interrupt)
 	} else {
+		ct.claimLogger = nil
 		nhns = ct.makeNameHashNext(names, false, interrupt)
 	}
 
 	for nhn := range nhns {
 		ct.merkleTrie.Update(nhn.Name, nhn.Hash, false)
+		if ct.claimLogger != nil {
+			ct.claimLogger.LogName(nhn.Name)
+		}
 	}
 }
 
