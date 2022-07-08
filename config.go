btcsuite/btcd

[lbry] version: update codebase to use version package

config.go
@@ -32,6 +32,7 @@ import (
 	_ "github.com/lbryio/lbcd/database/ffldb"
 	"github.com/lbryio/lbcd/mempool"
 	"github.com/lbryio/lbcd/peer"
+	"github.com/lbryio/lbcd/version"
 	"github.com/lbryio/lbcd/wire"
 	btcutil "github.com/lbryio/lbcutil"
 )
@@ -469,7 +470,7 @@ func loadConfig() (*config, []string, error) {
 	appName = strings.TrimSuffix(appName, filepath.Ext(appName))
 	usageMessage := fmt.Sprintf("Use %s -h to show usage", appName)
 	if preCfg.ShowVersion {
-		fmt.Println(appName, "version", version())
+		fmt.Println(appName, "version", version.Full())
 		os.Exit(0)
 	}
 

rpcserver.go
@@ -42,6 +42,7 @@ import (
 	"github.com/lbryio/lbcd/mining/cpuminer"
 	"github.com/lbryio/lbcd/peer"
 	"github.com/lbryio/lbcd/txscript"
+	"github.com/lbryio/lbcd/version"
 	"github.com/lbryio/lbcd/wire"
 	btcutil "github.com/lbryio/lbcutil"
 )
@@ -2451,7 +2452,7 @@ func handleGetHeaders(s *rpcServer, cmd interface{}, closeChan <-chan struct{}) 
 func handleGetInfo(s *rpcServer, cmd interface{}, closeChan <-chan struct{}) (interface{}, error) {
 	best := s.cfg.Chain.BestSnapshot()
 	ret := &btcjson.InfoChainResult{
-		Version:         int32(1000000*appMajor + 10000*appMinor + 100*appPatch),
+		Version:         version.Numeric(),
 		ProtocolVersion: int32(maxProtocolVersion),
 		Blocks:          best.Height,
 		TimeOffset:      int64(s.cfg.TimeSource.Offset().Seconds()),
@@ -2750,7 +2751,7 @@ func handleGetNetworkInfo(s *rpcServer, cmd interface{}, closeChan <-chan struct
 
 	reply := &btcjson.GetNetworkInfoResult{
 		ProtocolVersion: int32(wire.ProtocolVersion),
-		Version:         versionNumeric(),
+		Version:         version.Numeric(),
 		Connections:     s.cfg.ConnMgr.ConnectedCount(),
 		IncrementalFee:  cfg.MinRelayTxFee,
 		LocalAddresses:  localAddrs,

server.go
@@ -39,6 +39,7 @@ import (
 	"github.com/lbryio/lbcd/netsync"
 	"github.com/lbryio/lbcd/peer"
 	"github.com/lbryio/lbcd/txscript"
+	"github.com/lbryio/lbcd/version"
 	"github.com/lbryio/lbcd/wire"
 	"github.com/lbryio/lbcutil"
 	btcutil "github.com/lbryio/lbcutil"
@@ -71,7 +72,7 @@ var (
 
 	// userAgentVersion is the user agent version and is used to help
 	// identify ourselves to other bitcoin peers.
-	userAgentVersion = fmt.Sprintf("%d.%d.%d", appMajor, appMinor, appPatch)
+	userAgentVersion = version.Full()
 )
 
 // zeroHash is the zero value hash (all zeros).  It is defined as a convenience.

service_windows.go
@@ -10,6 +10,8 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/lbryio/lbcd/version"
+
 	"github.com/btcsuite/winsvc/eventlog"
 	"github.com/btcsuite/winsvc/mgr"
 	"github.com/btcsuite/winsvc/svc"
@@ -36,7 +38,7 @@ var elog *eventlog.Log
 // been started to the Windows event log.
 func logServiceStartOfDay(srvr *server) {
 	var message string
-	message += fmt.Sprintf("Version %s\n", version())
+	message += fmt.Sprintf("Version %s\n", version.Full())
 	message += fmt.Sprintf("Configuration directory: %s\n", defaultHomeDir)
 	message += fmt.Sprintf("Configuration file: %s\n", cfg.ConfigFile)
 	message += fmt.Sprintf("Data directory: %s\n", cfg.DataDir)

kodxana/lbcd

config.go
@@ -30,6 +30,7 @@ import (
 	_ "github.com/btcsuite/btcd/database/ffldb"
 	"github.com/btcsuite/btcd/mempool"
 	"github.com/btcsuite/btcd/peer"
+	"github.com/btcsuite/btcd/version"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/go-socks/socks"
@@ -470,7 +471,7 @@ func loadConfig() (*config, []string, error) {
 	appName = strings.TrimSuffix(appName, filepath.Ext(appName))
 	usageMessage := fmt.Sprintf("Use %s -h to show usage", appName)
 	if preCfg.ShowVersion {
-		fmt.Println(appName, "version", version())
+		fmt.Println(appName, "version", version.Full())
 		os.Exit(0)
 	}
 

rpcserver.go
@@ -41,6 +41,7 @@ import (
 	"github.com/btcsuite/btcd/mining/cpuminer"
 	"github.com/btcsuite/btcd/peer"
 	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/version"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/websocket"
 	"github.com/lbryio/lbcd/btcec"
@@ -2441,7 +2442,7 @@ func handleGetHeaders(s *rpcServer, cmd interface{}, closeChan <-chan struct{}) 
 func handleGetInfo(s *rpcServer, cmd interface{}, closeChan <-chan struct{}) (interface{}, error) {
 	best := s.cfg.Chain.BestSnapshot()
 	ret := &btcjson.InfoChainResult{
-		Version:         int32(1000000*appMajor + 10000*appMinor + 100*appPatch),
+		Version:         version.Numeric(),
 		ProtocolVersion: int32(maxProtocolVersion),
 		Blocks:          best.Height,
 		TimeOffset:      int64(s.cfg.TimeSource.Offset().Seconds()),
@@ -2740,7 +2741,7 @@ func handleGetNetworkInfo(s *rpcServer, cmd interface{}, closeChan <-chan struct
 
 	reply := &btcjson.GetNetworkInfoResult{
 		ProtocolVersion: int32(wire.ProtocolVersion),
-		Version:         versionNumeric(),
+		Version:         version.Numeric(),
 		Connections:     s.cfg.ConnMgr.ConnectedCount(),
 		IncrementalFee:  cfg.MinRelayTxFee,
 		LocalAddresses:  localAddrs,

server.go
@@ -41,6 +41,7 @@ import (
 	"github.com/btcsuite/btcd/netsync"
 	"github.com/btcsuite/btcd/peer"
 	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/version"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/decred/dcrd/lru"
 )
@@ -71,7 +72,7 @@ var (
 
 	// userAgentVersion is the user agent version and is used to help
 	// identify ourselves to other bitcoin peers.
-	userAgentVersion = fmt.Sprintf("%d.%d.%d", appMajor, appMinor, appPatch)
+	userAgentVersion = version.Full()
 )
 
 // zeroHash is the zero value hash (all zeros).  It is defined as a convenience.

service_windows.go
@@ -10,6 +10,8 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/btcsuite/btcd/version"
+
 	"github.com/btcsuite/winsvc/eventlog"
 	"github.com/btcsuite/winsvc/mgr"
 	"github.com/btcsuite/winsvc/svc"
@@ -36,7 +38,7 @@ var elog *eventlog.Log
 // been started to the Windows event log.
 func logServiceStartOfDay(srvr *server) {
 	var message string
-	message += fmt.Sprintf("Version %s\n", version())
+	message += fmt.Sprintf("Version %s\n", version.Full())
 	message += fmt.Sprintf("Configuration directory: %s\n", defaultHomeDir)
 	message += fmt.Sprintf("Configuration file: %s\n", cfg.ConfigFile)
 	message += fmt.Sprintf("Data directory: %s\n", cfg.DataDir)